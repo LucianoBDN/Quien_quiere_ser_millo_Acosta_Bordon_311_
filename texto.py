f9156a8f851823887e28a3386c68aec87a882ec6
Pio <pioacosta25@gmail.com>
2024-07-05 20:00:33 -0300

    más documentación (final)

--- texto.py
+++ texto.py
@@ -1,19 +1,33 @@
 import pygame
 
-def escribir_texto(ventana, texto, fuente, color_texto, x, y):
+def escribir_texto(ventana, texto: str, fuente: str, color_texto: tuple, x: int, y: int):
+    """Escribe texto en pantalla
+
+    Args:
+        ventana (surface): pantalla en la que escribimos
+        texto (str): texto que deseamos escribir
+        fuente (str): fuente para el texto
+        color_texto (tuple): color asignado al texto
+        x (int): ubicación en eje x del texto
+        y (int): ubicación en eje y del texto
+    """
 
     txt = fuente.render(texto, True, color_texto)
 
     ventana.blit(txt,(x,y))
 
 
-def escribir_pregunta(ventana, texto, fuente, color_texto, x, y):
+def renderizar_texto(ventana, fuente: str, texto: str, x: int, y: int, ancho_max: int):
+    """Se encarga de recortar el texto en distintas lineas para que no supere el ancho máximo, y luego lo muestra en pantalla
 
-    txt = fuente.render(texto, True, color_texto)
-
-    ventana.blit(txt,(x,y))
-
-def renderizar_texto(ventana, fuente, texto, x, y, ancho_max):
+    Args:
+        ventana (surface): pantalla donde muestra el texto 
+        fuente (str): fuente para el texto
+        texto (str): texto que deseamos mostrar
+        x (int): ubicación del texto en eje x
+        y (int): ubicación del texto en eje y
+        ancho_max (int): ancho máximo a respetar por el texto
+    """
     palabras = texto.split(' ')
     lineas = []
     linea_actual = ""
@@ -31,7 +45,16 @@ def renderizar_texto(ventana, fuente, texto, x, y, ancho_max):
         texto_renderizado = fuente.render(linea, True, (0, 0, 0))
         ventana.blit(texto_renderizado, (x, y + i * fuente.get_linesize()))
 
-def mostrar_respuestas(ventana, fuente, lista_respuestas, x, y):
+def mostrar_respuestas(ventana, fuente: str, lista_respuestas: list[dict], x: int, y: int):
+    """Muestra las posibles respuestas
+
+    Args:
+        ventana (surface): pantalla donde se mostrarán las respuestas
+        fuente (str): fuente de las respuestas
+        lista_respuestas (list): lista donde se encuentran las posibles respuestas
+        x (int): posición en eje x para mostrar las respuestas
+        y (int): posición en eje y para mostrar las respuestas
+    """
     ancho_max = 400
 
     auxiliar = x
--- ventana.py
+++ ventana.py
@@ -1,8 +1,18 @@
 import pygame
 
 
-def setear_ventana(titulo, path_icono, ancho_ventana, alto_ventana):
+def setear_ventana(titulo, path_icono: str, ancho_ventana: int, alto_ventana: int):
+    """Inicializa la ventana con todas sus características
 
+    Args:
+        self.titulo (str): titulo de la ventana
+        self.path_icono (str): ruta del icono de la ventana
+        self.ancho_ventana (int): ancho de la ventana
+        self.alto_ventana (int): alto de la ventana
+
+    Returns:
+        ventana (surface): devuelve la ventana ya armada
+    """
     DIMENSION_VENTANA = (ancho_ventana, alto_ventana)
 
     ventana = pygame.display.set_mode((DIMENSION_VENTANA))
